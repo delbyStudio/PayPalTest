06bff4e59a7f556d0f42481c91bff0dca3672f7f
del Buono Davide <dlbdvd96@hotmail.it>
2015-11-08 18:25:35 +0100

    x

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,9 @@
-angular.module('starter.controllers', ['ionic', "firebase"])
+angular.module('starter.controllers', ['ionic'])
 
 .controller('DashCtrl', function($scope, PaypalService, $ionicPopup) {
-
-var ref = new Firebase("https://seguimi.firebaseio.com");
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    // the access token will allow us to make Open Graph API calls
-    $scope.email = authData.facebook.email;
-  }
-}, {
-  scope: "email" // the permissions requested
-});
-
-
-
 $scope.buy = function(){
     PaypalService.initPaymentUI().then(function () {
-          PaypalService.makePayment(1.00, "Gettoni GiftShop (x10)", 'del Buono', 'Via A.Sottile, n.11', null, 'San Severo', 'FG', '71016', 'IT').then(function(){
+          PaypalService.makePayment(1.00, "Gettoni GiftShop (x10)", 'del Buono', 'Via A.Sottile', , 'San Severo', 'FG', '71016', 'IT').then(function(){
               $ionicPopup.alert({
               title: '<span class="assertive">CONGRATULAZIONI</span>',
               template: 'Il pagamento è stato effettuato con successo!'
