7eb99620c1f492caae3a8521081f8c844205a762
del Buono Davide <dlbdvd96@hotmail.it>
2015-11-08 01:00:51 +0100

    x

--- js/controllers.js
+++ js/controllers.js
@@ -2,27 +2,27 @@ angular.module('starter.controllers', ['ionic'])
 
 .controller('DashCtrl', function($scope, $ionicPlatform) {
 
-var app = {
    // Application Constructor
-   initialize: function() {
-       this.bindEvents();
-   },
+   $scope.initialize = function() {
+       $scope.bindEvents();
+   }
    // Bind Event Listeners
    //
    // Bind any events that are required on startup. Common events are:
    // 'load', 'deviceready', 'offline', and 'online'.
-   bindEvents: function() {
+   $scope.bindEvents = function() {
        document.addEventListener('deviceready', this.onDeviceReady, false);
-   },
+   }
    // deviceready Event Handler
    //
    // The scope of 'this' is the event. In order to call the 'receivedEvent'
    // function, we must explicity call 'app.receivedEvent(...);'
-   onDeviceReady: function() {
-       app.receivedEvent('deviceready');
-   },
+   $scope.onDeviceReady = function() {
+       $scope.receivedEvent('deviceready');
+   }
+
    // Update DOM on a Received Event
-   receivedEvent: function(id) {
+   $scope.receivedEvent = function(id) {
        var parentElement = document.getElementById(id);
        var listeningElement = parentElement.querySelector('.listening');
        var receivedElement = parentElement.querySelector('.received');
@@ -33,69 +33,58 @@ var app = {
        console.log('Received Event: ' + id);
 
        // start to initialize PayPalMobile library
-       app.initPaymentUI();
-   },
-   initPaymentUI : function () {
+       $scope.initPaymentUI();
+   }
+
+   $scope.initPaymentUI = function () {
      var clientIDs = {
        "PayPalEnvironmentProduction": "ARg9zpxtPzJBg3iP-0NanqLHlY1pXiNhIUXt9HHnQIJgkicE5hFZM1PL5BN1tr8H9aZIsCMiqLOzZgWo",
        "PayPalEnvironmentSandbox": "ARAw-jmuQrKB8F8qaJIQMVuhMsRVP817c489PgjUFNE4dnMp83xms_tBjCA8-eNgdGTK-akbs0u-E8d2"
      };
-     PayPalMobile.init(clientIDs, app.onPayPalMobileInit);
+     PayPalMobile.init(clientIDs, $scope.onPayPalMobileInit());
 
-   },
-   onSuccesfulPayment : function(payment) {
+   }
+
+   $scope.onSuccesfulPayment = function(payment) {
      console.log("payment success: " + JSON.stringify(payment, null, 4));
-   },
-   onAuthorizationCallback : function(authorization) {
+   }
+
+   $scope.onAuthorizationCallback = function(authorization) {
      console.log("authorization: " + JSON.stringify(authorization, null, 4));
-   },
-   createPayment : function () {
+   }
+
+   $scope.createPayment = function () {
      // for simplicity use predefined amount
      // optional payment details for more information check [helper js file](https://github.com/paypal/PayPal-Cordova-Plugin/blob/master/www/paypal-mobile-js-helper.js)
      var paymentDetails = new PayPalPaymentDetails("50.00", "0.00", "0.00");
      var payment = new PayPalPayment("50.00", "USD", "Awesome Sauce", "Sale", paymentDetails);
      return payment;
-   },
-   configuration : function () {
+   }
+
+   $scope.configuration = function () {
      // for more options see `paypal-mobile-js-helper.js`
      var config = new PayPalConfiguration({merchantName: "My test shop", merchantPrivacyPolicyURL: "https://mytestshop.com/policy", merchantUserAgreementURL: "https://mytestshop.com/agreement"});
      return config;
-   },
-   onPrepareRender : function() {
-     // buttons defined in index.html
-     //  <button id="buyNowBtn"> Buy Now !</button>
-     //  <button id="buyInFutureBtn"> Pay in Future !</button>
-     //  <button id="profileSharingBtn"> ProfileSharing !</button>
-     var buyNowBtn = document.getElementById("buyNowBtn");
-     var buyInFutureBtn = document.getElementById("buyInFutureBtn");
-     var profileSharingBtn = document.getElementById("profileSharingBtn");
+   }
 
+   $scope.onPrepareRender = function() {
      $scope.buy = function(e) {
        // single payment
-       PayPalMobile.renderSinglePaymentUI(app.createPayment(), app.onSuccesfulPayment, app.onUserCanceled);
+       PayPalMobile.renderSinglePaymentUI($scope.createPayment(), $scope.onSuccesfulPayment, $scope.onUserCanceled);
      };
+   }
 
-     buyInFutureBtn.onclick = function(e) {
-       // future payment
-       PayPalMobile.renderFuturePaymentUI(app.onAuthorizationCallback, app.onUserCanceled);
-     };
-
-     profileSharingBtn.onclick = function(e) {
-       // profile sharing
-       PayPalMobile.renderProfileSharingUI(["profile", "email", "phone", "address", "futurepayments", "paypalattributes"], app.onAuthorizationCallback, app.onUserCanceled);
-     };
-   },
-   onPayPalMobileInit : function() {
+   $scope.onPayPalMobileInit = function() {
      // must be called
      // use PayPalEnvironmentNoNetwork mode to get look and feel of the flow
-     PayPalMobile.prepareToRender("PayPalEnvironmentSandbox", app.configuration(), app.onPrepareRender);
-   },
-   onUserCanceled : function(result) {
+     PayPalMobile.prepareToRender("PayPalEnvironmentSandbox", $scope.configuration(), $scope.onPrepareRender);
+   }
+
+   $scope.onUserCanceled = function(result) {
      console.log(result);
    }
-};
 
-app.initialize();
+   $scope.initialize();
 
 })
 
